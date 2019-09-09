015e51114ee8ad22bc8c20b096a25de35021021b
tomoyooo <yomomachi.machi9325@gmail.com>
2019-09-09 14:52:04 +0900

    スマホだとtouchstartイベントになるよう変更

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
+// (function() {
+//   'use strict';
+
+//   var card = document.getElementsByClassName('card');
+//   var btn = document.getElementsByClassName('btn');
+
+//   for(let i = 0; i < btn.length; i++){
+  
+//     //クリックイベントでアラートを表示する
+//     btn[i].addEventListener('click', () => {
+//       card[i].className = 'card open'    
+//   }, false);
+    
+//   }   
+    
+
+// })();
 (function() {
   'use strict';
 
   var card = document.getElementsByClassName('card');
   var btn = document.getElementsByClassName('btn');
+  var mytap = window.ontouchstart===null?"touchstart":"click";
 
   for(let i = 0; i < btn.length; i++){
   
     //クリックイベントでアラートを表示する
-    btn[i].addEventListener('click', () => {
-      card[i].className = 'card open'
-      
+    btn[i].addEventListener('mytap', () => {
+      card[i].className = 'card open'    
   }, false);
     
   }   
