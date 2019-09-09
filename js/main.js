fa3144500c5e09e9f7c70b8aad8b498103c2bc76
tomoyooo <yomomachi.machi9325@gmail.com>
2019-09-09 15:11:46 +0900

    うまくいかなかったため戻し

--- js/main.js
+++ js/main.js
@@ -1,31 +1,13 @@
-// (function() {
-//   'use strict';
-
-//   var card = document.getElementsByClassName('card');
-//   var btn = document.getElementsByClassName('btn');
-
-//   for(let i = 0; i < btn.length; i++){
-  
-//     //クリックイベントでアラートを表示する
-//     btn[i].addEventListener('click', () => {
-//       card[i].className = 'card open'    
-//   }, false);
-    
-//   }   
-    
-
-// })();
 (function() {
   'use strict';
 
-  var mytap = window.ontouchstart===null?"touchstart":"click";
   var card = document.getElementsByClassName('card');
   var btn = document.getElementsByClassName('btn');
 
   for(let i = 0; i < btn.length; i++){
   
     //クリックイベントでアラートを表示する
-    btn[i].addEventListener(mytap, () => {
+    btn[i].addEventListener('click', () => {
       card[i].className = 'card open'    
   }, false);
     
@@ -33,3 +15,4 @@
     
 
 })();
+
